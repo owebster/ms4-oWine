3fa6093a40388db93d48ed9cc4b731bbdf5bdcea
owebster <owainpw@gmail.com>
2021-07-20 20:22:00 +0000

    add shopping bag context

--- bag/urls.py
+++ bag/urls.py
@@ -1,7 +1,6 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_bag, name="view_bag")
+    path('', views.view_bag, name='view_bag')
 ]
