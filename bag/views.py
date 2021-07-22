fb81b062348c49c8544d3e0ab3074403f2fd5175
owebster <owainpw@gmail.com>
2021-07-22 21:26:27 +0000

    add shopping bag logic

--- bag/views.py
+++ bag/views.py
@@ -14,7 +14,7 @@ def add_to_bag(request, item_id):
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
-    if item_id in list*bag.keys()):
+    if item_id in list(bag.keys()):
         bag[item_id] += quantity
     else:
         bag[item_id] = quantity
