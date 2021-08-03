42c70cf15376848523aeb719f3d77d19280b0cfb
owebster <owainpw@gmail.com>
2021-08-03 21:08:25 +0000

    add initial stripe intergration

--- checkout/views.py
+++ checkout/views.py
@@ -7,13 +7,15 @@ from .forms import OrderForm
 def checkout(request):
     bag = request.session.get('bag', {})
     if not bag:
-        messages.error(request, "there's nothing in your bag at the moment")
+        messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
-    
+
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51JKUNAK1zA4AxQSnqUV69pgaXMfT9LLJ7mcvmVXsTWELjAvIBscSNXy1nsHXd7OaowfBHFRIYXx8wyYUzxL9r2ef00v3aV4KWF',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
