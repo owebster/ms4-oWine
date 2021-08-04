389921019fd08d2f81e09a2000b2967347119843
owebster <owainpw@gmail.com>
2021-08-04 20:45:58 +0000

    troubleshooting gitpod

--- checkout/views.py
+++ checkout/views.py
@@ -26,14 +26,17 @@ def checkout(request):
         currency=settings.STRIPE_CURRENCY,
     )
 
-    print(intent)
-
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51JKUNAK1zA4AxQSnqUV69pgaXMfT9LLJ7mcvmVXsTWELjAvIBscSNXy1nsHXd7OaowfBHFRIYXx8wyYUzxL9r2ef00v3aV4KWF',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
