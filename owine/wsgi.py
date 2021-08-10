87b48ef11cff68b4bd024555f42670470eb4ffca
owebster <owainpw@gmail.com>
2021-08-10 17:25:34 +0000

    adjusted owine name to camelcase

--- owine/wsgi.py
+++ owine/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for oWine project.
+WSGI config for owine project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oWine.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'owine.settings')
 
 application = get_wsgi_application()
