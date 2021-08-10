9813c5146fe4fa139dfaf659f21c21902ce3a0b5
owebster <owainpw@gmail.com>
2021-08-10 17:28:13 +0000

    change owine to camelcase in asgi.py

--- owine/asgi.py
+++ owine/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for oWine project.
+ASGI config for owine project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oWine.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'owine.settings')
 
 application = get_asgi_application()
