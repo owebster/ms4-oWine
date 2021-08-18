7d01c155533e1204723c70d8536b44a5c57dd190
owebster <owainpw@gmail.com>
2021-08-18 20:45:23 +0000

    add blog app to project

--- bag/views.py
+++ bag/views.py
@@ -10,6 +10,7 @@ def view_bag(request):
 
     return render(request, 'bag/bag.html')
 
+
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
@@ -39,7 +40,7 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
-    
+
 
 def adjust_bag(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
