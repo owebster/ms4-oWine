8709fac85b3bf363c2833d1c48672acef1b2b326
owebster <owainpw@gmail.com>
2021-08-20 21:01:50 +0000

    added down arrow to hero page

--- owine/settings.py
+++ owine/settings.py
@@ -221,7 +221,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'o@wine.com'
+    DEFAULT_FROM_EMAIL = 'hello@owine.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
